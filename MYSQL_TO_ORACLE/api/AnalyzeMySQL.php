292eec2a4b392f3282f07d1856969ef64244735e
Gillog <swgil007@naver.com>
2022-03-10 09:33:52 +0900

    add comment ddl list

--- MYSQL_TO_ORACLE/api/AnalyzeMySQL.php
+++ MYSQL_TO_ORACLE/api/AnalyzeMySQL.php
@@ -28,6 +28,16 @@ if($matched_table_name) {
 }
 $matched_comment_list = $MySQLAnalyzer->get_comment_list();
 if($matched_comment_list) {
+    $comment_ddl_head = "COMMENT ON COLUMN ";
+    $comment_ddl_tail = " IS '";
+    $comment_ddl = array();
+    echo "TEST";
+    foreach($matched_comment_list[1] as $key => $value) {
+        $temp_comment_ddl_item = $comment_ddl_head . $matched_table_name . "." . $matched_comment_list[1][$key] . $comment_ddl_tail . $matched_comment_list[2][$key] . "';";
+        $comment_ddl[] = $temp_comment_ddl_item;
+    }
+    echo json_encode($comment_ddl, JSON_UNESCAPED_UNICODE);
+    echo "TEST!!!!";
     echo "[COLUMN NAME] ";
     echo chr(10);
     echo "-----------";
@@ -44,8 +54,6 @@ if($matched_comment_list) {
     echo "-----------";
 }
 $remove_comment_ddl = $MySQLAnalyzer->remove_comment_in_ddl($MySQLAnalyzer->ddl);
-echo "[REMOVE COMMENT DDL] ";
-echo $remove_comment_ddl;
 
 $remove_casecade_update_ddl = $MySQLAnalyzer->remove_cascade_update_in_ddl($remove_comment_ddl);
 echo "[REMOVE CASCADE UPDATE DDL] ";
